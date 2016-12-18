d45455bf99a05847525f9e55f1a7738dd8d87e9f
SherylHohman <SherylHohman@users.noreply.github.com>
2016-12-18 14:33:58 -0800

    done: 1.2.2: Cost Function and Gradient

--- week3-LogisticRegression-ex2/ex2/costFunction.m
+++ week3-LogisticRegression-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,11 +21,11 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
 
+J = 1/m * sum( (-1 * y .* log(h)) - ((1-y) .* log(1 - h)) );
 
-
-
-
+grad = 1 / m * sum ( (h - y) .* X);
 
 % =============================================================
 
